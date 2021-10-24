8f1b81d194a760a3e4c3c5128fb49b6eff6f4d63
Rasool Nikravesh <nikravesh.rasool@outlook.com>
2021-10-24 20:28:10 +0330

    start

---
+++ MusicWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MusicWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
