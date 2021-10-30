40b230653715f38310d0ee0536f1ad54f11e741d
rasoolnikravesh <nikravesh.rasool@gmail.com>
2021-10-30 11:33:32 +0330

    Add Music Model To app #1

---
+++ MusicWebApp/Models/Music.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicWebApp.Models
+{
+    public class Music
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }    
+
+        public string Title { get; set; }
+
+        public string Url128 { get; set; }      
+        public string Url320 { get; set; }      
+    }
+}
