c7893c01b0f4b509943249c98f61e57281481268
rasoolnikravesh <nikravesh.rasool@gmail.com>
2021-10-30 12:02:00 +0330

    Music Class Model update #2

--- MusicWebApp/Models/Music.cs
+++ MusicWebApp/Models/Music.cs
@@ -12,7 +12,8 @@ namespace MusicWebApp.Models
         public string Name { get; set; }    
 
         public string Title { get; set; }
-
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
         public string Url128 { get; set; }      
         public string Url320 { get; set; }     
         public Singer Singer { get; set;} 
