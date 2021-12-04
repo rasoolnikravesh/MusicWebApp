90b61bc5756d7e973bd63f6b1b4acca3379ccfd4
rasoolnikravesh <nikravesh.rasool@gmail.com>
2021-12-04 17:12:26 +0330

    copmlate artistautomaper

--- MusicWebApp/Mapers/ArtistMaperProfile.cs
+++ MusicWebApp/Mapers/ArtistMaperProfile.cs
@@ -30,8 +30,30 @@ namespace MusicWebApp.Mapers
         {
             CreateMap<InsertArtistViewModel, Artist>()
             .ForMember(x => x.Singer, y => y.MapFrom(z => singer(z.IsSinger)))
-            .ForMember(x => x.SongWriter, y => y.MapFrom(z => writer(z.IsSongWriter)));
+            .ForMember(x => x.SongWriter, y => y.MapFrom(z => writer(z.IsSongWriter)))
+            .ForMember(x => x.Arrangement, y => y.MapFrom(z => areng(z.IsArrengement)))
+            .ForMember(x => x.Compos, y => y.MapFrom(z => comp(z.IsComposer)))
+            .ForMember(x => x.RemixMusics, y => y.MapFrom(z => mix(z.IsMixAndMaster)))
+            .ReverseMap();
 
+        }
+
+        private object mix(bool isMixAndMaster)
+        {
+            if (isMixAndMaster) return new MixMaster();
+            else return null;
+        }
+
+        private Composer comp(bool isComposer)
+        {
+            if (isComposer) return new Composer();
+            else return null;
+        }
+
+        private Arrangement areng(bool isArrengement)
+        {
+            if (isArrengement) return new Arrangement();
+            else return null;
         }
     }
 }
